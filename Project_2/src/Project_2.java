65d0eb8e83a02401c1497015d3c618640d315bdb
jonathanb650 <19ballonasanchezj03@millshighsch.org>
2017-08-24 09:30:37 -0700

    I've been waiting for this to work!!!!!!!!!!!

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,12 @@
+//Jonathan Ballona
+//August 23, 2017
+//Project 2 -- Use print and println
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello there");
+		System.out.println("Hello there");
+
+	}
+
+}
