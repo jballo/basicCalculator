82c8882043f3d6e58e8b31ed91ccc4c416887703
Jonathan <Jonathan@192.168.1.124>
2017-09-10 15:41:02 -0700

    Calculating Library

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -20,6 +20,6 @@ public class DoMath {
 		System.out.println(Calculate.max(-9.731, 9.01));
 		System.out.println(Calculate.max(7.2, -0.01, 10));
 		System.out.println(Calculate.min(6, 9));
+		//System.out.println(Calculate.round2(2.798));
 	}
-
 }
